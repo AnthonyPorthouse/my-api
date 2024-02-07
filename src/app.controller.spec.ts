48ea0e36c03c5de94bd64dd01d333ef690faa3b7
Anthony Porthouse <anthony.porthouse@bbc.co.uk>
2024-02-07 15:51:43 +0000

    test: improve broken test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return a valid "Hello World!" API response', () => {
+      expect(appController.getHello()).toStrictEqual({
+        data: { message: 'Hello, World!' },
+      });
     });
   });
 });
